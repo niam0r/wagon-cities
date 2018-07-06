4c5213a186653a6ece238f495c18b2cad647d99f
Romain niam0r <niamor@mm.st>
2018-07-06 16:01:55 +0100

    changed City in a stateless component

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
-
-import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import { selectCity } from "../actions";
 
-class City extends Component {
-  handleClick = () => {
-    this.props.selectCity(this.props.city);
-  }
-
-  render() {
-    return (
-      <div className="list-group-item" onClick={this.handleClick} >
-        {this.props.city.name}
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    selectedCity: state.selectedCity
-  };
+const City = (props) => {
+  return (
+    <li
+      className="list-group-item"
+      onClick={() => props.selectCity(props.city)}
+    >
+      {props.city.name}
+    </li>
+  );
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ selectCity }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(City);
+export default connect(null, mapDispatchToProps)(City);
 
